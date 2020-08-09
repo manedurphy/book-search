9199a1d4c0f7c221ddf432e41b93f89f3853026d
Dane Murphy <danemurphy@Danes-MacBook-Pro.local>
2020-08-09 02:04:36 -0700

    changed logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '.././../assets/images/foodLogo.jpg';
+import logo from '../../assets/pokerLogo.png';
 import { Link } from 'react-router-dom';
 import '../../index.scss';
 
